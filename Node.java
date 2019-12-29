4cbf1eec9e0aa646a4df598dc4e60e9fc3cd05eb
emerson19292 <emersonsdnz@outlook.com>
2019-12-29 20:12:11 +1300

    Add files via upload

---
+++ Node.java
@@ -0,0 +1,15 @@
+package aStar;
+
+public class Node {
+
+	int x, y;
+	
+	int parentX, parentY;
+	
+	int fCost, gCost, hCost;
+	
+	public Node() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
